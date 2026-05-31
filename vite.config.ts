d6dce7015f15efcd23ce5438f373a38d414ba93a
Julio Espinoza <espinozajc@gmail.com>
2026-05-31 13:19:04 -0400

    Configure custom domain for Implementator

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/implementator/',
+  base: './',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
   },
 });
-
